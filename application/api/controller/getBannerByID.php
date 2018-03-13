741990c32da7355b60ad31fd3d9d6cc4c5dc664c
txwsing <yhlnszbd@aliyun.com>
2018-03-13 17:35:26 +0800

    test1

---
+++ application/api/controller/getBannerByID.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by 飞刀又见飞刀.
+ * User: Mryin
+ * Date: 2018/3/13
+ * Time: 17:27
+ * Company :郑州云客汇网络科技有限公司
+ */
+
+namespace app\api\controller;
+
+
+class getBannerByID
+{
+
+}
