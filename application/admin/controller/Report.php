a35fc9a8ddd0b3b3aa90237d21763cd4a03c9daf
txwsing <yhlnszbd@aliyun.com>
2018-03-15 18:45:07 +0800

    2018

---
+++ application/admin/controller/Report.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by 飞刀又见飞刀.
+ * User: Mryin
+ * Date: 2018/3/15
+ * Time: 18:10
+ * Company :郑州云客汇网络科技有限公司
+ */
+
+namespace app\admin\controller;
+
+
+use think\Controller;
+
+class Report extends Controller
+{
+
+}
